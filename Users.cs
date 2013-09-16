57420b741b9ad4a89e492bdda95a4aea53dc7e05
RamKrish <rakrish90@gmail.com>
2013-09-16 11:13:43 +0530

    Create Users.cs

---
+++ Users.cs
@@ -0,0 +1,11 @@
+
+namespace Library
+{
+    public class Users
+    {
+        private string strName;
+        private string strAddress;
+        private string strUserName;
+        private string strPassword;
+    }
+}
